e468eb3d16c2b4ed5f2328776de5609d195c0a27
Peter Kim <tweaked540@gmail.com>
2015-10-02 14:16:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#make sure to install Veil and SMBExec from the THP 2 Setup Phase first
 pip install termcolor
 wget thehackerplaybook.com/Download/winworder.zip
 unzip winworder.zip
+cp -R ./usr/share/pyinstaller /opt/pyinstaller-2.0
